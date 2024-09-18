03266473de4a8aaa95ba1804265432bfb32f46be
Yaroslav <71347933+superhe11@users.noreply.github.com>
2024-09-18 16:48:08 +0300

    Revert "feat: added JS implementation of the main functionality"
    This reverts commit 3c589d5ec62015040e4ce7df33da44bb3e378fa3.

--- scripts/selector.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('theme-switch').addEventListener('change', function () {
-  const toggler = document.querySelector('.theme-toggler');
-  if (this.checked) {
-    toggler.classList.add('theme-toggler-active');
-  } else {
-    toggler.classList.remove('theme-toggler-active');
-  }
-});
